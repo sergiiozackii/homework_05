b30b58a6de298fd4a4b82e975e1c20b26a29e53e
sergii.ozackii <46957601+sergiiozackii@users.noreply.github.com>
2019-02-04 21:59:09 +0200

    Add files via upload

---
+++ DoWhileSeven.java
@@ -0,0 +1,11 @@
+package _01;
+
+public class DoWhileSeven {
+    public static void main(String []args){
+        int count=1;
+        while (++count<=15){
+            System.out.println("Номер числа:"+count+","+"число "+count*7);
+
+        }
+    }
+}
