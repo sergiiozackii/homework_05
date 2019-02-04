f5798f96fe5206f0997a3f9faa52274f70426fa5
sergii.ozackii <46957601+sergiiozackii@users.noreply.github.com>
2019-02-05 01:33:02 +0200

    Update DoWhileSeven.java

--- DoWhileSeven.java
+++ DoWhileSeven.java
@@ -2,7 +2,7 @@ package _04;
 
 public class DoWhileSeven {
     public static void main(String []args){
-        int count=1;
+        int count=0;
         while (++count<=15){
             System.out.println("Номер числа:"+count+","+"число "+count*7);
 
