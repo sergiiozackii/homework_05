4081487c070570285627d610d044b8315f63e027
sergii.ozackii <46957601+sergiiozackii@users.noreply.github.com>
2019-02-05 01:26:22 +0200

    Update DigitForSeven.java

--- DigitForSeven.java
+++ DigitForSeven.java
@@ -2,7 +2,7 @@ package _04;
 
 public class DigitForSeven {
     public static void main(String[]args){
-        int count=1;
+        int count=0;
        for(;count<=15;count++){
            System.out.println("Номер числа:"+count+","+"число "+count*7);
 }
